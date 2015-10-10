12043ed3acb12d4ea3b4fadf7361cd04454dcb94
Mara Reyes <marareyes@athena.local>
2015-10-10 17:20:28 +0800

    initial home, contacts and events page

--- what2gift/both/router.js
+++ what2gift/both/router.js
@@ -7,3 +7,15 @@ Router.map(function() {
     path: '/'
   });
 });
+
+Router.route('/contacts', {
+    name: 'contacts',
+    template: 'contactsList',
+    layoutTemplate: 'layout',
+});
+
+Router.route('/events', {
+    name: 'events',
+    template: 'eventsList',
+    layoutTemplate: 'layout',
+});
