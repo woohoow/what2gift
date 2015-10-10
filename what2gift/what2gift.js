3695f0f089d66ced09cdc6337d638b0fbe717dc7
Dax Reyes <dax@amajang.local>
2015-10-10 16:01:17 +0800

    home welcome in ionic

--- what2gift/what2gift.js
+++ what2gift/what2gift.js
@@ -2,22 +2,16 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+  Template.home.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
-  Template.hello.events({
+  Template.home.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
