1f978e0ca4d46de349ad252127f509dc7d409103
Mara Reyes <moowa@users.noreply.github.com>
2015-10-11 09:07:55 +0800

    removed default home click counter

--- what2gift/client/what2gift.js
+++ what2gift/client/what2gift.js
@@ -1,16 +1,3 @@
-Template.home.helpers({
-    counter: function() {
-        return Session.get('counter');
-    }
-});
-
-Template.home.events({
-    'click button': function() {
-        // increment the counter when button is clicked
-        Session.set('counter', Session.get('counter') + 1);
-    }
-});
-
 Template.events_list.helpers({
     'events': function() {
         return Events.find({
