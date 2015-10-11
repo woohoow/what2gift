cbfbe49961e4cc4d5ab269e17bb27bbcb2d4caf9
Mara Reyes <moowa@users.noreply.github.com>
2015-10-11 14:24:21 +0800

    added users in fixtures

--- what2gift/server/fixtures.js
+++ what2gift/server/fixtures.js
@@ -1,13 +1,64 @@
 var POPULATE = {};
 
+POPULATE.users = function() {
+    //sample users
+    if (Meteor.users.find().count() === 0) {
+        console.log('Creating Users');
+
+        Accounts.createUser({
+            "username": "admin1",
+            "email": "admin1@loc.al",
+            "password": "password",
+        });
+
+        for (var i = 0; i < 4; i++) {
+            var user = "user" + i;
+            Accounts.createUser({
+                "username": user,
+                "email": user + "@loc.al",
+                "password": "password",
+            });
+        }
+
+        var user1 = _.pick(Meteor.users.findOne({
+            username: "user1"
+        }), 'username', '_id');
+        var user2 = _.pick(Meteor.users.findOne({
+            username: "user2"
+        }), 'username', '_id');
+        var user3 = _.pick(Meteor.users.findOne({
+            username: "user3"
+        }), 'username', '_id');
+        console.log('users', user1, user2, user3);
+
+    }
+}
+
+
+
 POPULATE.events = function() {
     //sample events
 
     if (Events.find().count() === 0) {
         console.log('Creating Sample Events');
-        Events.insert({
+        var user1 = Meteor.users.findOne({
+            username: "user1"
+        }, {
+            fields: {
+                username: 1,
+                _id: 1
+            }
+        });
+
+        console.log("creating event for user", user1);
+
+
+        Events._collection.insert({
+            _id: Random.id(),
+            user: user1,
             name: 'Christmas 2014',
             when: new Date("2014-12-25T00:00:00Z"),
+            status: 'active',
             for: ['Mo'],
             items : [ {
                 "name": "Ball",
@@ -43,4 +94,5 @@ POPULATE.events = function() {
     }
 };
 
+POPULATE.users();
 POPULATE.events();
